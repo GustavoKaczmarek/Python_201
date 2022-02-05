5cbdc061e21d551cb36efd795097ec5de6cdd26e
Gustavo Ramirez <gustavo_ramirez@outlook.com>
2022-02-05 17:08:53 +0800

    Terminé la versión básica de hangman

--- list_comprenhensions.py
+++ list_comprenhensions.py
@@ -1,10 +1,16 @@
 def run():
-    squares = []
-    for i in range(1, 101):
-        if i % 3 != 0:
-            squares.append(i**2)
+    #squares = []
+    #for i in range(1, 101):
+    #    if i % 3 != 0:
+    #        squares.append(i**2)
 
-    print(squares)
+    # squares = [i**2 for i in range(1,101) if i % 3 != 0]
+
+    # squares = [i**2 for i in range (1,1000) if i % 4 == 0 and i % 6 == 0 and i % 9 == 0 and i**2 < 100000]
+
+    my_list = [i for i in range(1,100000) if i % 4 == 0 and i % 6 == 0 and i % 9 == 0]
+
+    print(my_list)
 
 
 if __name__ == '__main__':
